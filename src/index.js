0343f9148c8a621d943f5f5866eb36496e885e2e
Oleksandr <110254286+OleksandrUA777@users.noreply.github.com>
2023-03-18 18:05:08 +0100

    add

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import { GlobalStyle } from 'GlobalStyle';
-// import { AppWrap } from 'components/App/App.styled';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
